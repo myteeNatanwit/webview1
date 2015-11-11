8ba41337e605512efd14cfe6a008c36ae8b67a9f
Bee <beetran21@yahoo.com>
2015-11-10 19:56:28 -0800

    don't know what it is

---
+++ webview1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  webview1
+//
+//  Created by Michael Tran on 10/11/2015.
+//  Copyright © 2015 intcloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webview: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
